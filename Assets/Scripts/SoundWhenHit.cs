fe2bc16ba3f8f532b62c3ef9f6a62936455ea9c3
kaikue <kvk@pobox.com>
2018-06-05 13:10:29 -0400

    Make sound volume based on hit velocity

--- Assets/Scripts/SoundWhenHit.cs
+++ Assets/Scripts/SoundWhenHit.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class SoundWhenHit : MonoBehaviour {
 
+    private const float SOUND_SCALE = 0.5f;
+
+    private AudioSource audioSrc;
+    private float originalVolume;
+
+    private void Start()
+    {
+        audioSrc = gameObject.GetComponent<AudioSource>();
+        originalVolume = audioSrc.volume;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
-        print("GONG");
-        gameObject.GetComponent<AudioSource>().Play();
+        audioSrc.volume = collision.relativeVelocity.magnitude * SOUND_SCALE * originalVolume;
+        print(gameObject + " BONK " + audioSrc.volume);
+        audioSrc.Play();
     }
 }
