134de19067c88b859f79190fbbde2f3d7d8ef1f4
jkcao <jkcao@users.noreply.github.com>
2018-06-07 14:22:06 -0400

    Code clean-up on menu and timer

--- Assets/Scripts/PreMeditationMenu.cs
+++ Assets/Scripts/PreMeditationMenu.cs
@@ -13,7 +13,6 @@ public class PreMeditationMenu : MonoBehaviour
     public string nextScene;
 
     private float WELCOME_LOAD_TIME = 2f;
-    private float FADE_TIME = 0.5f; //TODO: add fading transitions between menu parts
 
     private void Start()
     {
@@ -23,46 +22,55 @@ public class PreMeditationMenu : MonoBehaviour
     private IEnumerator Welcome()
     {
         yield return new WaitForSeconds(WELCOME_LOAD_TIME);
+
         this.gameObject.transform.Find("Welcome").gameObject.SetActive(false);
         this.gameObject.transform.Find("Time").gameObject.SetActive(true);
     }
 
 	private void Update()
 	{
-        if(timeSelect.IsActive()) {
-            if(timeSelect.AnyTogglesOn()) {
-                string timeActive = timeSelect.ActiveToggles().FirstOrDefault().name;
-                switch (timeActive)
-                {
-                    case "5":
-                        MUserSettings.setTime(300f);
-                        break;
-                    case "10":
-                        MUserSettings.setTime(600f);
-                        break;
-                    case "20":
-                        MUserSettings.setTime(1200f);
-                        break;
-                    case "inf":
-                        MUserSettings.setTime(-1f); //Use negative value to indicate infinite time.
-                        break;
-                }
-
-                this.gameObject.transform.Find("Time").gameObject.SetActive(false);
-                this.gameObject.transform.Find("Environment").gameObject.SetActive(true);
-            }
-        }
-        if (environSelect.IsActive())
-        {
-            if (environSelect.AnyTogglesOn())
-            {
-                //Set-up, to potentially add different environments user can choose from.
-                MUserSettings.setEnviron(environSelect.ActiveToggles().FirstOrDefault().name);
-
-                this.gameObject.transform.Find("Environment").gameObject.SetActive(false);
-
-                SceneManager.LoadScene(nextScene);
-            }
-        }
+        if (timeSelect.IsActive()) setTime();
+        if (environSelect.IsActive()) setEnvironment();
 	}
+
+    private void setTime()
+    {
+        if (timeSelect.AnyTogglesOn())
+        {
+            string timeActive = timeSelect.ActiveToggles().FirstOrDefault().name;
+            switch (timeActive)
+            {
+                case "5":
+                    MUserSettings.setTime(300f);
+                    break;
+                case "10":
+                    MUserSettings.setTime(600f);
+                    break;
+                case "20":
+                    MUserSettings.setTime(1200f);
+                    break;
+                case "inf":
+                    MUserSettings.setTime(-1f); //Use negative value to indicate infinite time.
+                    break;
+            }
+
+            this.gameObject.transform.Find("Time").gameObject.SetActive(false);
+            this.gameObject.transform.Find("Environment").gameObject.SetActive(true);
+        }
+    }
+
+    private void setEnvironment()
+    {
+        if (environSelect.AnyTogglesOn())
+        {
+            //Set-up, to potentially add different environments user can choose from.
+            MUserSettings.setEnviron(environSelect.ActiveToggles().FirstOrDefault().name);
+
+            this.gameObject.transform.Find("Environment").gameObject.SetActive(false);
+
+            //Move into the room, since set-up is complete.
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+
 }
--- Assets/Scripts/SoundTimer.cs
+++ Assets/Scripts/SoundTimer.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class SoundTimer : MonoBehaviour {
 
     public int time; //in seconds.
-    public float fadeSpeed; //how fast audio should fade out.
     public GameObject musicSource;
     public GameObject soundSource;
 
@@ -13,8 +12,8 @@ public class SoundTimer : MonoBehaviour {
     private AudioSource musicAudio;
     private AudioSource soundAudio;
     private bool startFade;
+    private float FADE_SPEED = 0.5f;
 
-	// Use this for initialization
 	void Start () {
         musicAudio = musicSource.GetComponent<AudioSource>();
         soundAudio = soundSource.GetComponent<AudioSource>();
@@ -27,19 +26,20 @@ public class SoundTimer : MonoBehaviour {
         while(timeLeft > 0) {
             yield return new WaitForSeconds(1.0f);
             timeLeft--;
-            Debug.Log(timeLeft);
         }
+        //Fade music out once time is up.
         StartCoroutine(Fade());
     }
 
     private IEnumerator Fade()
     {
+        //TODO: prevent user from being able to adjust volume at this point.
         while(musicAudio.volume > 0 || soundAudio.volume > 0) {
-            yield return new WaitForSeconds(0.005f);
-            if(musicAudio.volume > 0) musicAudio.volume -= 0.005f * fadeSpeed;
-            if(soundAudio.volume > 0) soundAudio.volume -= 0.005f * fadeSpeed;
-
+            yield return new WaitForSeconds(0.01f);
+            if(musicAudio.volume > 0) musicAudio.volume -= 0.01f * FADE_SPEED;
+            if(soundAudio.volume > 0) soundAudio.volume -= 0.01f * FADE_SPEED;
         }
+        //Remove components since no more sound will be playing for the rest of the time.
         Destroy(musicSource);
         Destroy(soundSource);
     }
