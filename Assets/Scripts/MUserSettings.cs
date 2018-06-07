feb369dc2962a0b9e684b30678c1861cfb8a4706
jkcao <jkcao@users.noreply.github.com>
2018-06-07 10:11:24 -0400

    Some menu adjustments
    Added saving environment choice (for future) and some aesthetic changes

---
+++ Assets/Scripts/MUserSettings.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MUserSettings
+{
+    private static float time; //in seconds.
+    private static string environ = "ocean";
+
+    public static float getTime() {
+        return time;
+    }
+
+    public static void setTime(float newTime)
+    {
+        time = newTime;
+    }
+
+    public static string getEnviron()
+    {
+        return environ;
+    }
+
+    public static void setEnviron(string newEnviron)
+    {
+        environ = newEnviron;
+    }
+}
