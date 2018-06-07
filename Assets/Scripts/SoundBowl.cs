697a543afcb299ea04fb90f0e66c9421cdff1ada
kaikue <kvk@pobox.com>
2018-06-07 10:41:06 -0400

    Smooth volume transition

--- Assets/Scripts/SoundBowl.cs
+++ Assets/Scripts/SoundBowl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class SoundBowl : Bowl {
 
+    private const float VOLUME_TRANSITION_TIME = 1.0f;
+
     public int maxObjects;
 
     private AudioSource audioSrc;
@@ -15,6 +17,17 @@ public class SoundBowl : Bowl {
 
     protected override void Refresh()
     {
-        audioSrc.volume = ((float)numObjects) / maxObjects;
+        StartCoroutine(TransitionToVolume(((float)numObjects) / maxObjects));
+    }
+
+    private IEnumerator TransitionToVolume(float newVolume)
+    {
+        float oldVolume = audioSrc.volume;
+        for (float t = 0; t < VOLUME_TRANSITION_TIME; t += Time.deltaTime)
+        {
+            audioSrc.volume = Mathf.Lerp(oldVolume, newVolume, t / VOLUME_TRANSITION_TIME);
+            yield return new WaitForEndOfFrame();
+        }
+        audioSrc.volume = newVolume;
     }
 }
