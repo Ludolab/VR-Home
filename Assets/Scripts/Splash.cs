e436287d4adac804efc33b450d80ce6e34e49263
Kai Kuehner <kvk@pobox.com>
2018-06-12 17:44:31 -0400

    Fade sinking stone faster

--- Assets/Scripts/Splash.cs
+++ Assets/Scripts/Splash.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Splash : MonoBehaviour
 {
 
+    private const float FADE_SPEED = 0.03f;
+
+    public GameObject fallingStone;
+
     private AudioSource audioSrc;
+    private Renderer stoneRenderer;
 
 	private void Start()
     {
         audioSrc = GetComponent<AudioSource>();
         audioSrc.Play();
+        stoneRenderer = fallingStone.GetComponent<Renderer>();
 	}
 
     private void Update()
     {
+        Color oldCol = stoneRenderer.material.color;
+        stoneRenderer.material.color = new Color(oldCol.r, oldCol.g, oldCol.b, oldCol.a - FADE_SPEED);
+
         if (!audioSrc.isPlaying)
         {
             Destroy(gameObject);
