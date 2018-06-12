cc894567d1841bde076d974b099bfc99e199739e
JacquiwithaQ <jfashimpaur@gmail.com>
2018-06-12 12:24:06 -0400

    Candle Flicker

---
+++ Assets/Scripts/CandleFlicker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandleFlicker : MonoBehaviour {
+
+    public float flickerProbability;
+    public float minIntensity;
+    public float maxIntensity;
+    public Light flameLight;
+
+	// Use this for initialization
+	void Start () {
+        flameLight.intensity = minIntensity;
+	}
+	
+	// Update is called once per fram
+ 
+    void Update()
+    {
+        float liklihood = Random.Range(1, 100);
+        if (liklihood < flickerProbability){
+            flameLight.intensity = Random.Range(minIntensity, maxIntensity);
+        }
+    }
+}
