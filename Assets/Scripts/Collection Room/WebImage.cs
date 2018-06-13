9fd0781b19085622eb5ae3c5ad180b441abc12ca
Kai Kuehner <kvk@pobox.com>
2018-06-13 15:57:56 -0400

    add more nice pictures :-)

--- Assets/Scripts/Collection Room/WebImage.cs
+++ Assets/Scripts/Collection Room/WebImage.cs
@@ -1,30 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Net;
 using UnityEngine;
 
 public class WebImage : MonoBehaviour
 {
 
-    private const string IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/2012_Suedchinesischer_Tiger.JPG/220px-2012_Suedchinesischer_Tiger.JPG";
+    public string imageUrl;
 
     private Renderer rend;
-
+    private Vector2 originalScale;
+    
     private void Start()
     {
+        originalScale = transform.localScale;
         rend = GetComponent<Renderer>();
         StartCoroutine(DownloadImage());
     }
 
     private IEnumerator DownloadImage()
     {
-        using (WWW www = new WWW(IMAGE_URL))
+        using (WWW www = new WWW(imageUrl))
         {
             yield return www;
 
-            rend.material.mainTexture = www.texture;
+            Texture2D texture = www.texture;
+            SetTexture(texture);
         }
     }
-    
+
+    private void SetTexture(Texture2D texture)
+    {
+        float width = originalScale.x;
+        float height = originalScale.y;
+
+        float aspect = (float)texture.width / texture.height;
+        print(aspect);
+        if (aspect > 1)
+        {
+            height /= aspect;
+        }
+        else
+        {
+            width *= aspect;
+        }
+
+        transform.localScale = new Vector3(width, height, 1);
+        rend.material.mainTexture = texture;
+    }
+
 }
