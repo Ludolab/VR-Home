e0efcdb940029198cb302da787bcb9d0dff87ed8
Kai Kuehner <kvk@pobox.com>
2018-06-13 14:48:20 -0400

    The Tiger Room

---
+++ Assets/Scripts/Collection Room/WebImage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net;
+using UnityEngine;
+
+public class WebImage : MonoBehaviour
+{
+
+    private const string IMAGE_URL = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/2012_Suedchinesischer_Tiger.JPG/220px-2012_Suedchinesischer_Tiger.JPG";
+
+    private Renderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        StartCoroutine(DownloadImage());
+    }
+
+    private IEnumerator DownloadImage()
+    {
+        using (WWW www = new WWW(IMAGE_URL))
+        {
+            yield return www;
+
+            rend.material.mainTexture = www.texture;
+        }
+    }
+    
+}
