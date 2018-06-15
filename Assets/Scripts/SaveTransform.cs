0ff41bb2679a9006ebff513289c89be2ee8acdba
jkcao <jkcao@users.noreply.github.com>
2018-06-15 15:44:16 -0400

    Save transforms of objects in scene

---
+++ Assets/Scripts/SaveTransform.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveTransform {
+    [SerializeField] public string objName;
+    [SerializeField] public float xPosition;
+    [SerializeField] public float yPosition;
+    [SerializeField] public float zPosition;
+    [SerializeField] public float xScale;
+    [SerializeField] public float yScale;
+    [SerializeField] public float zScale;
+}
