8390ec15bebc073126414700c1592ff673f39737
jkcao <jkcao@users.noreply.github.com>
2018-06-19 15:49:07 -0400

    more saving work + deleted useless folders

---
+++ Assets/Scripts/Saving/ID.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Could be changedto be something for unquie later, but should function for now.
+[System.Serializable]
+public class ID  {
+    [SerializeField] public string objName;
+    [SerializeField] public float objCoordX;
+}
--- Assets/Scripts/Saving/UpdatedObjects.cs
+++ Assets/Scripts/Saving/UpdatedObjects.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class CreatedPrefabs {
+public static class UpdatedObjects {
 
     private static Dictionary<int, string> createdObj = new Dictionary<int, string>();
+    private static List<int> destroyedObj = new List<int>();
 
     public static void addToCreated(int created, string from) {
         createdObj.Add(created, from);
@@ -12,5 +13,13 @@ public static class CreatedPrefabs {
 
     public static Dictionary<int, string> getCreatedObj() {
         return createdObj;
+    }
+
+    public static void addToDestroyed(int destroyed) {
+        destroyedObj.Add(destroyed);
+    }
+
+    public static List<int> getDestroyedObj() {
+        return destroyedObj;
     }
 }
