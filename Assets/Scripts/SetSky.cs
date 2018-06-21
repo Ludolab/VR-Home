f0cf8da28b68b418a7ed3cee8724c3f4dbcfd979
JacquiwithaQ <jfashimpaur@gmail.com>
2018-06-21 16:30:39 -0400

    Added Sun Movement (needs work)

--- Assets/Scripts/SetSky.cs
+++ Assets/Scripts/SetSky.cs
@@ -11,8 +11,11 @@ public class SetSky : MonoBehaviour {
     public Color daylightColor = Color.white; //new Color(1, 0.9568f, 0.8392f, 1);
     public Color sunsetColor = Color.red;//new Color(1, 0.9682f, 0.6462f, 1);
     public Color nightColor = Color.blue;//new Color(0.946f, 0.929f, 1, 1);
-    public Light sun;
+    public Light sunLight;
     public Light bounceLight;
+    public GameObject sun;
+
+    private Vector3 sunDefaultPositionVector = new Vector3(-10, 0, -135);
 
 	// Use this for initialization
     void Start () {
@@ -23,7 +26,7 @@ public class SetSky : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         /* MANUAL TIME SHIFT: FOR DEBUGGING */
-        if (Input.GetKeyDown("right"))
+        if (Input.GetKey("right"))
         {
             percentThroughDay += 1f;
             if (percentThroughDay > 100)
@@ -32,7 +35,7 @@ public class SetSky : MonoBehaviour {
             }
             //Debug.Log("percentThroughDay is " + percentThroughDay);
         }
-        if (Input.GetKeyDown("left"))
+        if (Input.GetKey("left"))
         {
             percentThroughDay -= 1f;
             if (percentThroughDay < 0)
@@ -53,11 +56,12 @@ public class SetSky : MonoBehaviour {
             RenderSettings.skybox = sunsetToNight;
             sunsetToNight.SetFloat("_Blend", percentThroughEvening);
             float angleVertical = Mathf.Lerp(20, -5, percentThroughEvening);
-            sun.transform.rotation = Quaternion.Euler(angleVertical, angleLateral, angleVertical);
-            sun.intensity = Mathf.Lerp(1.5f, 0, percentThroughEvening);
-            sun.color = sunsetColor;
+            sunLight.transform.rotation = Quaternion.Euler(angleVertical, angleLateral, angleVertical);
+            sun.transform.position = sunLight.transform.rotation * sunDefaultPositionVector;
+            sunLight.intensity = Mathf.Lerp(1.5f, 0, percentThroughEvening);
+            sunLight.color = sunsetColor;
             bounceLight.color = Color.Lerp(sunsetColor, nightColor, percentThroughEvening);
-            bounceLight.intensity = Mathf.Lerp(sun.intensity/2, 0.0f, percentThroughEvening);
+            bounceLight.intensity = Mathf.Lerp(sunLight.intensity/2, 0.0f, percentThroughEvening);
         }
         else
         // Before Sunset    
@@ -66,12 +70,13 @@ public class SetSky : MonoBehaviour {
             RenderSettings.skybox = dayToSunset;
             dayToSunset.SetFloat("_Blend", percentThroughMorning);
             float angleVertical = Mathf.Lerp(60, 20, percentThroughMorning);
-            sun.transform.rotation = Quaternion.Euler(angleVertical, angleLateral, angleVertical);
-            sun.intensity = 1.5f;
+            sunLight.transform.rotation = Quaternion.Euler(angleVertical, angleLateral, angleVertical);
+            sun.transform.position = sunLight.transform.rotation * sunDefaultPositionVector;
+            sunLight.intensity = 1.5f;
             Color lightColor = Color.Lerp(daylightColor, sunsetColor, percentThroughMorning);
-            sun.color = lightColor;
+            sunLight.color = lightColor;
             bounceLight.color = lightColor;
-            bounceLight.intensity = sun.intensity/2;
+            bounceLight.intensity = sunLight.intensity/2;
         }
     }
 
