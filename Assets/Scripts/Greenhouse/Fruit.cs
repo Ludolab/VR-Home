947f6cbaeb95321053205e0cfc2acb702d94bf7c
Kai Kuehner <kvk@pobox.com>
2018-07-02 16:02:23 -0400

    joints

--- Assets/Scripts/Greenhouse/Fruit.cs
+++ Assets/Scripts/Greenhouse/Fruit.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Leap.Unity.Interaction;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,14 +7,17 @@ public class Fruit : MonoBehaviour
 {
 
     private ConfigurableJoint joint;
+    private InteractionBehaviour ib;
 
     private void Start()
     {
         joint = GetComponent<ConfigurableJoint>();
+        ib = GetComponent<InteractionBehaviour>();
     }
 
     public void Release()
     {
         Destroy(joint);
+        ib.RefreshPositionLockedState();
     }
 }
