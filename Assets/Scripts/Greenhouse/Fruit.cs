3603169ca9eed51b3b0b608681c5020f2a345422
Kai Kuehner <kvk@pobox.com>
2018-07-02 15:31:08 -0400

    Remove fruit when grabbing

---
+++ Assets/Scripts/Greenhouse/Fruit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+
+    private ConfigurableJoint joint;
+
+    private void Start()
+    {
+        joint = GetComponent<ConfigurableJoint>();
+    }
+
+    public void Release()
+    {
+        print("RELEASE");
+        Destroy(joint);
+    }
+}
