fbfdb3fa32006c5eaea8cd7de9bdca74008235fc
Kai Kuehner <kvk@pobox.com>
2018-07-03 16:41:29 -0400

    beetle collision

---
+++ Assets/Scripts/Greenhouse/Beetle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Beetle : MonoBehaviour
+{
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		print("COLLISION ENTER " + collision.gameObject);
+	}
+
+	private void OnCollisionStay(Collision collision)
+	{
+		print("COLLISION STAY " + collision.gameObject);
+	}
+}
