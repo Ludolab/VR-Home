ca7d0e84e2a9f2cadb731e7e9f180c2d8ba45cb6
Kai Kuehner <kvk@pobox.com>
2018-07-03 17:30:36 -0400

    flick those beetles

--- Assets/Scripts/Greenhouse/Beetle.cs
+++ Assets/Scripts/Greenhouse/Beetle.cs
@@ -5,13 +5,48 @@ using UnityEngine;
 public class Beetle : MonoBehaviour
 {
 
-	private void OnCollisionEnter(Collision collision)
+	public float flickThreshold;
+	public float flickMultiplier;
+
+	private Rigidbody rb;
+
+	private bool isFlicked;
+
+	private void Start()
 	{
-		print("COLLISION ENTER " + collision.gameObject);
+		rb = GetComponent<Rigidbody>();
+		isFlicked = false;
 	}
 
 	private void OnCollisionStay(Collision collision)
 	{
-		print("COLLISION STAY " + collision.gameObject);
+		bool isLeapHand = collision.gameObject.name.StartsWith("Contact");
+
+		if (!isFlicked && isLeapHand)
+		{
+			Vector3 vel = collision.gameObject.GetComponent<Rigidbody>().velocity;
+			if (vel.magnitude > flickThreshold)
+			{
+				isFlicked = true;
+				Flick(vel);
+			}
+		}
+		if (isFlicked && !isLeapHand)
+		{
+			Squish();
+		}
+	}
+
+	private void Flick(Vector3 vel)
+	{
+		rb.isKinematic = false;
+		rb.useGravity = true;
+		rb.AddForce(vel * flickMultiplier, ForceMode.Impulse);
+	}
+
+	private void Squish()
+	{
+		//TODO: sound, particles
+		Destroy(gameObject);
 	}
 }
