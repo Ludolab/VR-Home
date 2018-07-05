d15c68cf3b36e71f514f328b5999acde1e5e592f
Kai Kuehner <kvk@pobox.com>
2018-07-05 17:30:48 -0400

    enable letter in envelope when end is torn off

---
+++ Assets/Scripts/Greenhouse/TearOff.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TearOff : MonoBehaviour
+{
+
+	public Letter letter;
+
+	private void OnJointBreak(float breakForce)
+	{
+		letter.JointBroke();
+	}
+}
