4ea409a43d8a05ab4f786a45b67dbc448d7327c2
JeenaYin <634540750@qq.com>
2018-07-09 15:36:40 -0400

    button sounds

---
+++ Assets/Scripts/Greenhouse/buttonSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonSound : MonoBehaviour {
+
+  AudioSource button;
+  public AudioClip pressSoundClip;
+	public AudioClip unpressSoundClip;
+
+	// Use this for initialization
+	void Start () {
+		button = GetComponent<AudioSource>();
+	}
+
+	public void pressSound() {
+		button.clip = pressSoundClip;
+		button.Play();
+	}
+	public void unpressSound() {
+		button.clip = unpressSoundClip;
+		button.Play();
+	}
+}
