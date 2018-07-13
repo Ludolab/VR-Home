a6a8cffa2763c2393add691881c80d4472cbe4f9
jkcao <jkcao@users.noreply.github.com>
2018-07-13 15:07:15 -0400

    all the plant prefabs
    to put some beetle boys and fruit boys on those plant boys

--- Assets/Scripts/Greenhouse/Plant.cs
+++ Assets/Scripts/Greenhouse/Plant.cs
@@ -6,6 +6,12 @@ public class Plant : MonoBehaviour
 {
 
 	public GameObject[] growthStages;
+    public Transform[] growthStagesTrans; //List of transforms corresponding to the growth stages.
+    public bool multiHarvest; //For plants that bear fruit that can be picked without taking the entire plant.
+    public GameObject fruit; //Model of fruit (if multiHarvest plant).
+    public Transform[] fruitTrans; //Where fruit can spawn (if multiHarvest plant), locally.
+    public Transform[] beetleTransYoung; //Where beetles can spawn, on young stage, locally.
+    public Transform[] beetleTransGrown; //Where beetles can spawn, on young stage, locally.
 	public int nonFruitingStages; //1 for seed only, 2 for seed and one non-fruiting young stage, etc.
 
 	private GameObject model;
@@ -26,7 +32,7 @@ public class Plant : MonoBehaviour
 		{
 			Destroy(model);
 		}
-		model = Instantiate(growthStages[stage], transform.position, Quaternion.identity);
+        model = Instantiate(growthStages[stage], growthStagesTrans[stage]);
 
 		//TODO: spawn fruit- half ripe and half unripe (don't spawn unripe if last ripe one was not picked)
 	}
