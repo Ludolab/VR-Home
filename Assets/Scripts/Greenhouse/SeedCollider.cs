8a469401a5fcbef2cd6fab1a6f7f8a97bef3ea16
JacquiwithaQ <jfashimpaur@gmail.com>
2018-07-17 14:43:23 -0400

    Maybe Plots work now maybe

---
+++ Assets/Scripts/Greenhouse/SeedCollider.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeedCollider : MonoBehaviour {
+    
+    public GameObject myStarter;
+    public Dirt myDirt;
+
+	// Use this for initialization
+	void Start () {
+        myStarter = null;
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if (other.gameObject.GetComponent<Starter>() != null && myStarter == null){
+            myStarter = other.gameObject;
+        }
+        if (myDirt.digState == 1){
+            StartCoroutine(myDirt.TakePlant());
+        }
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+        if (other.gameObject.GetComponent<Starter>() != null)
+        {
+            myStarter = null;
+        }
+        myDirt.digState = 1;
+	}
+}
