fcf99926816645eaec1607ac239eafb20b18a5ff
Kai Kuehner <kvk@pobox.com>
2018-07-19 14:30:24 -0400

    Reset position when spacebar pressed

---
+++ Assets/Scripts/Greenhouse/ResetPosition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour
+{
+
+	private Vector3 initialPos;
+	private Quaternion initialRot;
+
+	private void Start()
+	{
+		initialPos = transform.position;
+		initialRot = transform.rotation;
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Reset();
+		}
+	}
+
+	private void Reset()
+	{
+		transform.position = initialPos;
+		transform.rotation = initialRot;
+	}
+}
