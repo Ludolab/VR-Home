7331d22017aff5c5f008421338f89339c7af9c7a
Kai Kuehner <kvk@pobox.com>
2018-08-06 12:00:29 -0400

    Fix image quad scaling

--- Assets/Scripts/Collection Room/ImageQuad.cs
+++ Assets/Scripts/Collection Room/ImageQuad.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
 
 public class ImageQuad : MediaQuad
@@ -26,8 +28,44 @@ public class ImageQuad : MediaQuad
 
     public void SetTexture(Texture2D texture)
     {
-        Scale(texture.width, texture.height);
+		//Get image dimensions- this is a pain due to Unity fitting textures to powers of 2 with no easy way to get original dimensions
+		int texWidth;
+		int texHeight;
+		bool success = GetImageSize(texture, out texWidth, out texHeight);
+		if (!success)
+		{
+			//automatically getting dimensions failed- use less accurate method
+			texWidth = texture.width;
+			texHeight = texture.height;
+		}
+		
+		Scale(texWidth, texHeight);
         rend.material.mainTexture = texture;
     }
 
+	//By numberkruncher
+	//https://forum.unity.com/threads/getting-original-size-of-texture-asset-in-pixels.165295/
+	public static bool GetImageSize(Texture2D asset, out int width, out int height)
+	{
+		if (asset != null)
+		{
+			string assetPath = AssetDatabase.GetAssetPath(asset);
+			TextureImporter importer = AssetImporter.GetAtPath(assetPath) as TextureImporter;
+
+			if (importer != null)
+			{
+				object[] args = new object[2] { 0, 0 };
+				MethodInfo mi = typeof(TextureImporter).GetMethod("GetWidthAndHeight", BindingFlags.NonPublic | BindingFlags.Instance);
+				mi.Invoke(importer, args);
+
+				width = (int)args[0];
+				height = (int)args[1];
+
+				return true;
+			}
+		}
+
+		height = width = 0;
+		return false;
+	}
 }
